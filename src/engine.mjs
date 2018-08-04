eb375ebb8964774230072e2fb6377a7a78ad45a4
Gus Caplan <me@gus.host>
2018-08-04 16:09:33 -0500

    fix running

--- src/engine.mjs
+++ src/engine.mjs
@@ -4,7 +4,6 @@
 import type {
   Value,
   PrimitiveValue,
-  UndefinedValue,
   SymbolValue,
   ObjectValue,
   FunctionValue,
@@ -18,6 +17,7 @@ import type {
 */
 
 import {
+  UndefinedValue,
   New as NewValue,
   Type,
 } from './value.mjs';
