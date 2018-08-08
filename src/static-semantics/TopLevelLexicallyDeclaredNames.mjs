eda802b7c747139d51495c36ce05b7f8ed3ea1e5
Timothy Gu <timothygu99@gmail.com>
2018-08-08 00:57:46 -0400

    LexicallyScopedDeclarations done

--- src/static-semantics/TopLevelLexicallyDeclaredNames.mjs
+++ src/static-semantics/TopLevelLexicallyDeclaredNames.mjs
@@ -1,8 +1,38 @@
+import {
+  isDeclaration,
+  isHoistableDeclaration,
+  isStatement,
+} from '../ast.mjs';
+import {
+  BoundNames_Declaration,
+} from './BoundNames.mjs';
+
 // 13.2.7 #sec-block-static-semantics-toplevellexicallydeclarednames
 //   StatementList : StatementList StatementListItem
 //
 // (implicit)
 //   StatementList : StatementListItem
 export function TopLevelLexicallyDeclaredNames_StatementList(StatementList) {
-  return [];
+  const names = [];
+  for (const StatementListItem of StatementList) {
+    names.push(...TopLevelLexicallyDeclaredNames_StatementListItem(StatementListItem));
+  }
+  return names;
+}
+
+// 13.2.7 #sec-block-static-semantics-toplevellexicallydeclarednames
+//   StatementListItem : Statement
+//   StatementListItem : Declaration
+export function TopLevelLexicallyDeclaredNames_StatementListItem(StatementListItem) {
+  switch (true) {
+    case isStatement(StatementListItem):
+      return [];
+    case isDeclaration(StatementListItem):
+      if (isHoistableDeclaration(StatementListItem)) {
+        return [];
+      }
+      return BoundNames_Declaration(StatementListItem);
+    default:
+      throw new TypeError(`Unexpected StatementListItem: ${StatementListItem.type}`);
+  }
 }
--- src/static-semantics/TopLevelLexicallyScopedDeclarations.mjs
+++ src/static-semantics/TopLevelLexicallyScopedDeclarations.mjs
@@ -1,8 +1,35 @@
-// 15.1.4 #sec-scripts-static-semantics-lexicallyscopeddeclarations
+import {
+  isDeclaration,
+  isHoistableDeclaration,
+  isStatement,
+} from '../ast.mjs';
+
+// 13.2.8 #sec-block-static-semantics-toplevellexicallyscopeddeclarations
 //   StatementList : StatementList StatementListItem
 //
 // (implicit)
 //   StatementList : StatementListItem
 export function TopLevelLexicallyScopedDeclarations_StatementList(StatementList) {
-  return [];
+  const declarations = [];
+  for (const StatementListItem of StatementList) {
+    declarations.push(...TopLevelLexicallyScopedDeclarations_StatementListItem(StatementListItem));
+  }
+  return declarations;
+}
+
+// 13.2.8 #sec-block-static-semantics-toplevellexicallyscopeddeclarations
+//   StatementListItem : Statement
+//   StatementListItem : Declaration
+export function TopLevelLexicallyScopedDeclarations_StatementListItem(StatementListItem) {
+  switch (true) {
+    case isStatement(StatementListItem):
+      return [];
+    case isDeclaration(StatementListItem):
+      if (isHoistableDeclaration(StatementListItem)) {
+        return [];
+      }
+      return [StatementListItem];
+    default:
+      throw new TypeError(`Unexpected StatementListItem: ${StatementListItem.type}`);
+  }
 }
