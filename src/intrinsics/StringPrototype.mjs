f2adfd802ea93451b66ff69d1c93f9c5e3543ad1
Gus Caplan <me@gus.host>
2018-08-14 17:24:18 -0500

    add some string prototype

--- src/intrinsics/StringPrototype.mjs
+++ src/intrinsics/StringPrototype.mjs
@@ -1,1 +1,90 @@
-export function CreateStringPrototype(realmRec) {}
+import {
+  surroundingAgent,
+} from '../engine.mjs';
+import {
+  Assert,
+  RequireObjectCoercible,
+  ToString,
+  ToInteger,
+  CreateBuiltinFunction,
+} from '../abstract-ops/all.mjs';
+import {
+  StringExoticValue,
+  Type,
+  New as NewValue,
+} from '../value.mjs';
+import { Q } from '../completion.mjs';
+
+function thisStringValue(value) {
+  if (Type(value) === 'String') {
+    return value;
+  }
+  if (Type(value) === 'Object' && 'StringData' in value) {
+    Assert(Type(value.StringData) === 'String');
+    return value.StringData;
+  }
+  return surroundingAgent.TypeError('TypeError');
+}
+
+function StringProto_charAt(realm, [pos], { thisValue }) {
+  const O = Q(RequireObjectCoercible(thisValue));
+  const S = Q(ToString(O));
+  const position = Q(ToInteger(pos)).numberValue();
+  const size = S.stringValue().length;
+  if (position < 0 || position >= size) {
+    return NewValue('');
+  }
+  return NewValue(S.stringValue()[position]);
+}
+
+function StringProto_charCodeAt(realm, [pos], { thisValue }) {
+  const O = Q(RequireObjectCoercible(thisValue));
+  const S = Q(ToString(O));
+  const position = Q(ToInteger(pos)).numberValue();
+  const size = S.stringValue().length;
+  if (position < 0 || position >= size) {
+    return NewValue(NaN);
+  }
+  return S.stringValue().charCodeAt(position);
+}
+
+function StringProto_toString(realm, args, { thisValue }) {
+  return Q(thisStringValue(thisValue));
+}
+function StringProto_valueOf(realm, args, { thisValue }) {
+  return Q(thisStringValue(thisValue));
+}
+
+export function CreateStringPrototype(realmRec) {
+  const proto = new StringExoticValue();
+  proto.StringData = NewValue('');
+
+  [
+    ['charAt', StringProto_charAt, 1],
+    ['charCodeAt', StringProto_charCodeAt, 1],
+    ['toString', StringProto_toString, 0],
+    ['valueOf', StringProto_valueOf, 0],
+  ].forEach((name, fn, length) => {
+    const n = CreateBuiltinFunction(fn, [], realmRec);
+    n.properties.set(NewValue('length'), {
+      Value: NewValue(length),
+      Writable: false,
+      Enumerable: false,
+      Configurable: false,
+    });
+    n.properties.set(NewValue('name'), {
+      Value: NewValue(name),
+      Writable: false,
+      Enumerable: false,
+      Configurable: false,
+    });
+    proto.DefineOwnProperty(NewValue(name), {
+      Value: n,
+      Writable: true,
+      Enumerable: false,
+      Configurable: true,
+    });
+  });
+
+  realmRec.Intrinsics['%StringPrototype%'] = proto;
+}
