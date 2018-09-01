5d6f6a64c27bef3da57d1e0081fae7143690e5ea
Timothy Gu <timothygu99@gmail.com>
2018-09-01 11:17:45 -0400

    Add ContainsUseStrict

---
+++ src/static-semantics/ContainsUseStrict.mjs
@@ -0,0 +1,28 @@
+import {
+  directivePrologueContainsUseStrictDirective,
+  isBlockStatement,
+  isExpression,
+} from '../ast.mjs';
+import { outOfRange } from '../helpers.mjs';
+
+// #sec-function-definitions-static-semantics-containsusestrict
+//   FunctionBody : FunctionStatementList
+export function ContainsUseStrict_FunctionBody(FunctionBody) {
+  return directivePrologueContainsUseStrictDirective(FunctionBody);
+}
+
+// #sec-arrow-function-definitions-static-semantics-containsusestrict
+//   ConciseBody : AssignmentExpression
+//
+// (implicit)
+//   ConciseBody : `{` FunctionBody `}`
+export function ContainsUseStrict_ConciseBody(ConciseBody) {
+  switch (true) {
+    case isExpression(ConciseBody):
+      return false;
+    case isBlockStatement(ConciseBody):
+      return ContainsUseStrict_FunctionBody(ConciseBody.body);
+    default:
+      throw outOfRange('ContainsUseStrict_ConciseBody', ConciseBody);
+  }
+}
