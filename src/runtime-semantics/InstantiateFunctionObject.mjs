670963de9bd78b795135ea93fc5ba0b9a40ab30e
Timothy Gu <timothygu99@gmail.com>
2018-09-27 19:55:51 -0700

    Formalize InstantiateFunctionObject

--- src/runtime-semantics/InstantiateFunctionObject.mjs
+++ src/runtime-semantics/InstantiateFunctionObject.mjs
@@ -4,18 +4,47 @@ import {
   MakeConstructor,
 } from '../abstract-ops/all.mjs';
 import {
+  isAsyncFunctionDeclaration,
+  isAsyncGeneratorDeclaration,
+  isFunctionDeclaration,
+  isGeneratorDeclaration,
+} from '../ast.mjs';
+import {
   New as NewValue,
 } from '../value.mjs';
 
-export function InstantiateFunctionObject(FunctionDeclaration, scope) {
+// #sec-function-definitions-runtime-semantics-instantiatefunctionobject
+//   FunctionDeclaration :
+//     `function` BindingIdentifier `(` FormalParameters `)` `{` FunctionBody `}`
+//     `function` `(` FormalParameters `)` `{` FunctionBody `}`
+export function InstantiateFunctionObject_FunctionDeclaration(FunctionDeclaration, scope) {
   const {
     id: BindingIdentifier,
     params: FormalParameters,
   } = FunctionDeclaration;
-  const strict = true;
+  const strict = true; // TODO(IsStrict)
   const name = NewValue(BindingIdentifier ? BindingIdentifier.name : 'default');
   const F = FunctionCreate('Normal', FormalParameters, FunctionDeclaration, scope, strict);
   MakeConstructor(F);
   SetFunctionName(F, name);
   return F;
+}
+
+export function InstantiateFunctionObject(AnyFunctionDeclaration, scope) {
+  switch (true) {
+    case isFunctionDeclaration(AnyFunctionDeclaration):
+      return InstantiateFunctionObject_FunctionDeclaration(AnyFunctionDeclaration, scope);
+
+    // case isGeneratorDeclaration(AnyFunctionDeclaration):
+    //   return InstantiateFunctionObject_GeneratorDeclaration(AnyFunctionDeclaration, scope);
+
+    // case isAsyncFunctionDeclaration(AnyFunctionDeclaration):
+    //   return InstantiateFunctionObject_AsyncFunctionDeclaration(AnyFunctionDeclaration, scope);
+
+    // case isAsyncGeneratorDeclaration(AnyFunctionDeclaration):
+    //   return InstantiateFunctionObject_AsyncGeneratorDeclaration(AnyFunctionDeclaration, scope);
+
+    default:
+      throw outOfRange('InstantiateFunctionObject', AnyFunctionDeclaration);
+  }
 }
