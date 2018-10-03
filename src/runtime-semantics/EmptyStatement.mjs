b4634d87b9adce27604f5905dc3c920d76528a6b
Timothy Gu <timothygu99@gmail.com>
2018-10-03 10:06:37 -0700

    Add missing annotation for Evaluate_EmptyStatement

--- src/runtime-semantics/EmptyStatement.mjs
+++ src/runtime-semantics/EmptyStatement.mjs
@@ -1,5 +1,7 @@
 import { NormalCompletion } from '../completion.mjs';
 
+// #sec-empty-statement-runtime-semantics-evaluation
+//   EmptyStatement : `;`
 export function Evaluate_EmptyStatement(/* EmptyStatement */) {
   return new NormalCompletion(undefined);
 }
