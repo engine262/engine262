55f4bf0774d84d0a4e4034a2fcf889644994cee5
Timothy Gu <timothygu99@gmail.com>
2018-10-11 09:30:02 -0700

    Add an Assert

--- src/abstract-ops/async-function-operations.mjs
+++ src/abstract-ops/async-function-operations.mjs
@@ -11,6 +11,7 @@ export function AsyncFunctionStart(promiseCapability, asyncFunctionBody) {
   asyncContext.codeEvaluationState = (function* resumer() {
     const result = EnsureCompletion(yield* Evaluate_FunctionBody(asyncFunctionBody));
     // Assert: If we return here, the async function either threw an exception or performed an implicit or explicit return; all awaiting is done.
+    Assert(surroundingAgent.runningExecutionContext === asyncContext);
     surroundingAgent.executionContextStack.pop();
     if (result.Type === 'normal') {
       Q(Call(promiseCapability.Resolve, Value.undefined, [Value.undefined]));
