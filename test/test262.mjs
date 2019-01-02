c2448e15f5069f8e002065bda1422aff136c005b
Michaël Zasso <targos@protonmail.com>
2019-01-02 15:09:03 +0100

    Implement String.prototype.startsWith
    Refs: https://github.com/devsnek/engine262/issues/22

--- test/test262.mjs
+++ test/test262.mjs
@@ -165,6 +165,7 @@ const readyTests = [
   'built-ins/String/prototype/includes/**/*.js',
   'built-ins/String/prototype/padEnd/**/*.js',
   'built-ins/String/prototype/padStart/**/*.js',
+  'built-ins/String/prototype/startsWith/**/*.js',
   'built-ins/String/prototype/toLowerCase/**/*.js',
   'built-ins/String/prototype/toUpperCase/**/*.js',
   'built-ins/TypedArray/**/*.js',
