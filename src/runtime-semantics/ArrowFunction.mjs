2ecdf5c384a70e9d5257648208635015041dc872
Timothy Gu <timothygu99@gmail.com>
2019-06-26 22:40:10 -0700

    Fix toString for async arrow functions
    See https://github.com/tc39/ecma262/commit/fea0b845e0
    
    See #85

--- src/runtime-semantics/ArrowFunction.mjs
+++ src/runtime-semantics/ArrowFunction.mjs
@@ -6,6 +6,8 @@ import { FunctionCreate, GetValue, sourceTextMatchedBy } from '../abstract-ops/a
 import { Q, ReturnCompletion } from '../completion.mjs';
 import { Evaluate } from '../evaluator.mjs';
 
+// 14.2.17 #sec-arrow-function-definitions-runtime-semantics-evaluation
+//   ArrowFunction : ArrowParameters `=>` ConciseBody
 export function Evaluate_ArrowFunction(ArrowFunction) {
   const { params: ArrowParameters } = ArrowFunction;
   const scope = surroundingAgent.runningExecutionContext.LexicalEnvironment;
@@ -16,6 +18,7 @@ export function Evaluate_ArrowFunction(ArrowFunction) {
 }
 
 // https://github.com/tc39/ecma262/pull/1406
+//   ExpressionBody : AssignmentExpression
 export function* Evaluate_ExpressionBody(ExpressionBody) {
   const AssignmentExpression = ExpressionBody;
   const exprRef = yield* Evaluate(AssignmentExpression);
--- src/runtime-semantics/AsyncArrowFunction.mjs
+++ src/runtime-semantics/AsyncArrowFunction.mjs
@@ -1,13 +1,19 @@
 import {
   surroundingAgent,
 } from '../engine.mjs';
+import { X } from '../completion.mjs';
 // import { CoveredFormalsList } from '../static-semantics/all.mjs';
-import { AsyncFunctionCreate } from '../abstract-ops/all.mjs';
+import { AsyncFunctionCreate, sourceTextMatchedBy } from '../abstract-ops/all.mjs';
 
-export function Evaluate_AsyncArrowFunction(ArrowFunction) {
-  const { params: ArrowParameters } = ArrowFunction;
+// 14.8.16 #sec-async-arrow-function-definitions-runtime-semantics-evaluation
+//   AsyncArrowFunction :
+//     `async` AsyncArrowBindingIdentifier `=>` AsyncConciseBody
+//     CoverCallExpressionAndAsyncArrowHead `=>` AsyncConciseBody
+export function Evaluate_AsyncArrowFunction(AsyncArrowFunction) {
+  const { params: ArrowFormalParameters } = AsyncArrowFunction;
   const scope = surroundingAgent.runningExecutionContext.LexicalEnvironment;
-  const parameters = ArrowParameters;
-  const closure = AsyncFunctionCreate('Arrow', parameters, ArrowFunction, scope);
+  const parameters = ArrowFormalParameters;
+  const closure = X(AsyncFunctionCreate('Arrow', parameters, AsyncArrowFunction, scope));
+  closure.SourceText = sourceTextMatchedBy(AsyncArrowFunction);
   return closure;
 }
