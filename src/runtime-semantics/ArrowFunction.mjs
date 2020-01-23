ce8e52eb0f0067b8d4772e092d0d9d45d0ef4a34
Gus Caplan <me@gus.host>
2020-01-23 00:21:20 -0800

    implement https://github.com/tc39/ecma262/commit/a879fd542718bcab6accd2d38964c410fffa28a9

--- src/runtime-semantics/ArrowFunction.mjs
+++ src/runtime-semantics/ArrowFunction.mjs
@@ -5,7 +5,7 @@ import { OrdinaryFunctionCreate, GetValue, sourceTextMatchedBy } from '../abstra
 import { Q, X, ReturnCompletion } from '../completion.mjs';
 import { Evaluate } from '../evaluator.mjs';
 
-// 14.2.17 #sec-arrow-function-definitions-runtime-semantics-evaluation
+// #sec-arrow-function-definitions-runtime-semantics-evaluation
 //   ArrowFunction : ArrowParameters `=>` ConciseBody
 export function Evaluate_ArrowFunction(ArrowFunction) {
   const { params: ArrowParameters } = ArrowFunction;
@@ -16,11 +16,14 @@ export function Evaluate_ArrowFunction(ArrowFunction) {
   return closure;
 }
 
-// https://github.com/tc39/ecma262/pull/1406
+// #sec-arrow-function-definitions-runtime-semantics-evaluation
 //   ExpressionBody : AssignmentExpression
 export function* Evaluate_ExpressionBody(ExpressionBody) {
   const AssignmentExpression = ExpressionBody;
+  // 1. Let exprRef be the result of evaluating |AssignmentExpression|.
   const exprRef = yield* Evaluate(AssignmentExpression);
+  // 2. Let exprValue be ? GetValue(exprRef).
   const exprValue = Q(GetValue(exprRef));
+  // 3. Return Completion { [[Type]]: return, [[Value]]: exprValue, [[Target]]: empty }.
   return new ReturnCompletion(exprValue);
 }
