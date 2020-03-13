6a63ed499385cd92107e0b1d078f2c0db3be996a
Gus Caplan <me@gus.host>
2020-03-13 16:28:02 -0700

    update import.meta based on stage 4 pr

--- src/runtime-semantics/MetaProperty.mjs
+++ src/runtime-semantics/MetaProperty.mjs
@@ -7,7 +7,7 @@ import {
   OrdinaryObjectCreate,
   CreateDataProperty,
 } from '../abstract-ops/all.mjs';
-import { AbstractModuleRecord } from '../modules.mjs';
+import { SourceTextModuleRecord } from '../modules.mjs';
 import { X } from '../completion.mjs';
 import { Type, Value } from '../value.mjs';
 import { OutOfRange } from '../helpers.mjs';
@@ -21,20 +21,33 @@ function Evaluate_NewTarget() {
 // https://tc39.es/proposal-import-meta/#sec-meta-properties
 // ImportMeta : `import` `.` `meta`
 function Evaluate_ImportMeta() {
+  // 1. Let module be GetActiveScriptOrModule().
   const module = GetActiveScriptOrModule();
-  Assert(module instanceof AbstractModuleRecord);
+  // 2. Assert: module is a Source Text Module Record;
+  Assert(module instanceof SourceTextModuleRecord);
+  // 3. Let importMeta be module.ImportMeta.
   let importMeta = module.ImportMeta;
-  if (importMeta === Value.undefined) {
-    importMeta = OrdinaryObjectCreate(Value.null);
+  // 4. If importMeta is empty, then
+  if (importMeta === undefined) {
+    // a. Set importMeta to ! OrdinaryObjectCreate(null).
+    importMeta = X(OrdinaryObjectCreate(Value.null));
+    // b. Let importMetaValues be ! HostGetImportMetaProperties(module).
     const importMetaValues = X(HostGetImportMetaProperties(module));
+    // c. For each Record { [[Key]], [[Value]] } p that is an element of importMetaValues,
     for (const p of importMetaValues) {
+      // i. Perform ! CreateDataPropertyOrThrow(importMeta, p.[[Key]], p.[[Value]]).
       X(CreateDataProperty(importMeta, p.Key, p.Value));
     }
+    // d. Perform ! HostFinalizeImportMeta(importMeta, module).
     X(HostFinalizeImportMeta(importMeta, module));
+    // e. Set module.[[ImportMeta]] to importMeta.
     module.ImportMeta = importMeta;
+    // f. Return importMeta.
     return importMeta;
   } else {
+    // a. Assert: Type(importMeta) is Object.
     Assert(Type(importMeta) === 'Object');
+    // b. Return importMeta.
     return importMeta;
   }
 }
