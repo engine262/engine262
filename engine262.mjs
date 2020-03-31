8c502b075bf3a9adbd9579d20c71173142cce260
GHA <gha@example.com>
2020-03-31 18:36:07 +0000

    autobuild

--- engine262.mjs
+++ engine262.mjs
@@ -1,5 +1,5 @@
 /*
- * engine262 0.0.1 00920d6be650ee76522dfd1f29d55044cc2b527d
+ * engine262 0.0.1 1493ad798f84b74d5c2cb0a249d7222320fa67f4
  *
  * Copyright (c) 2018 engine262 Contributors
  * 
@@ -50027,32 +50027,47 @@ function GeneratorResumeAbrupt(generator, abruptCompletion) {
   Assert(surroundingAgent.runningExecutionContext === methodContext, "surroundingAgent.runningExecutionContext === methodContext"); // 12. Return Completion(result).
 
   return Completion(result);
-} // 25.4.3.5 #sec-getgeneratorkind
+} // #sec-getgeneratorkind
 
 function GetGeneratorKind() {
-  const genContext = surroundingAgent.runningExecutionContext;
+  // 1. Let genContext be the running execution context.
+  const genContext = surroundingAgent.runningExecutionContext; // 2. If genContext does not have a Generator component, return non-generator.
 
   if (!genContext.Generator) {
     return 'non-generator';
-  }
+  } // 3. Let generator be the Generator component of genContext.
 
-  const generator = genContext.Generator;
+
+  const generator = genContext.Generator; // 4. If generator has an [[AsyncGeneratorState]] internal slot, return async.
 
   if ('AsyncGeneratorState' in generator) {
     return 'async';
-  }
+  } // 5. Else, return sync.
+
 
   return 'sync';
-} // 25.4.3.6 #sec-generatoryield
+} // #sec-generatoryield
 
 function* GeneratorYield(iterNextObj) {
-  const genContext = surroundingAgent.runningExecutionContext;
-  const generator = genContext.Generator;
-  Assert(GetGeneratorKind() === 'sync', "GetGeneratorKind() === 'sync'");
-  generator.GeneratorState = 'suspendedYield';
-  surroundingAgent.executionContextStack.pop(genContext);
-  const resumptionValue = yield new NormalCompletion(iterNextObj);
-  return resumptionValue;
+  // 1. Assert: iterNextObj is an Object that implements the IteratorResult interface.
+  // 2. Let genContext be the running execution context.
+  const genContext = surroundingAgent.runningExecutionContext; // 3. Assert: genContext is the execution context of a generator.
+
+  Assert(genContext.Generator !== undefined, "genContext.Generator !== undefined"); // 4. Let generator be the value of the Generator component of genContext.
+
+  const generator = genContext.Generator; // 5. Assert: GetGeneratorKind is sync.
+
+  Assert(GetGeneratorKind() === 'sync', "GetGeneratorKind() === 'sync'"); // 6. Set generator.GeneratorState to suspendedYield.
+
+  generator.GeneratorState = 'suspendedYield'; // 7. Remove genContext from the execution context stack.
+
+  surroundingAgent.executionContextStack.pop(genContext); // 8. Set the code evaluation state of genContext such that when evaluation is resumed with
+  //    a Completion resumptionValue the following steps will be performed:
+  //      a. Return resumptionValue
+
+  const resumptionValue = yield new NormalCompletion(iterNextObj); // 9. Return NormalCompletion(iterNextObj).
+
+  return resumptionValue; // 10. NOTE: this returns to the evaluation of the operation that had most previously resumed evaluation of genContext.
 }
 
 // 18 #sec-global-object
