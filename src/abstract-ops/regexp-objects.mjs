68650853fdafb9a052a0f55b5377ebf401cdf918
ExE Boss <3889017+ExE-Boss@users.noreply.github.com>
2020-07-25 09:15:00 +0200

    RegExp: Implement the legacy RegExp features proposal

--- src/abstract-ops/regexp-objects.mjs
+++ src/abstract-ops/regexp-objects.mjs
@@ -1,12 +1,13 @@
 import { surroundingAgent } from '../engine.mjs';
 import { ParsePattern, isLineTerminator } from '../parse.mjs';
-import { Descriptor, Value } from '../value.mjs';
+import { Descriptor, Type, Value } from '../value.mjs';
 import { Q, X } from '../completion.mjs';
 import { Evaluate_Pattern } from '../runtime-semantics/all.mjs';
 import {
   Assert,
   DefinePropertyOrThrow,
   OrdinaryCreateFromConstructor,
+  SameValue,
   Set,
   ToString,
 } from './all.mjs';
@@ -23,12 +24,39 @@ export class MatchRecord {
 
 // #sec-regexpalloc
 export function RegExpAlloc(newTarget) {
-  const obj = Q(OrdinaryCreateFromConstructor(newTarget, '%RegExp.prototype%', ['RegExpMatcher', 'OriginalSource', 'OriginalFlags']));
+  const featureLegacyRegExp = surroundingAgent.feature('legacy-regexp');
+
+  // 1. Let obj be ? OrdinaryCreateFromConstructor(newTarget, "%RegExpPrototype%", «[[RegExpMatcher]], [[OriginalSource]], [[OriginalFlags]]»).
+  const obj = Q(OrdinaryCreateFromConstructor(newTarget, '%RegExp.prototype%', ['RegExpMatcher', 'OriginalSource', 'OriginalFlags',
+    ...(featureLegacyRegExp ? ['Realm', 'LegacyFeaturesEnabled'] : [])]));
+
+  // https://tc39.es/proposal-regexp-legacy-features/#sec-legacy-regexpalloc
+  if (featureLegacyRegExp) {
+    // Let obj be ? OrdinaryCreateFromConstructor(newTarget, "%RegExpPrototype%", «[[RegExpMatcher]], [[OriginalSource]], [[OriginalFlags]], [[Realm]], [[LegacyFeaturesEnabled]]»).
+    // Let thisRealm be the current Realm Record.
+    const thisRealm = surroundingAgent.currentRealmRecord;
+
+    // Set obj.[[Realm]] to thisRealm.
+    obj.Realm = thisRealm;
+
+    // If SameValue(newTarget, thisRealm.[[Intrinsics]].[[%RegExp%]]) is true, then
+    if (SameValue(newTarget, thisRealm.Intrinsics['%RegExp%']) === Value.true) {
+      // Set obj.[[LegacyFeaturesEnabled]] to true.
+      obj.LegacyFeaturesEnabled = Value.true;
+    } else {
+      // Else, set obj.[[LegacyFeaturesEnabled]] to false.
+      obj.LegacyFeaturesEnabled = Value.false;
+    }
+  }
+
+  // 2. Perform ! DefinePropertyOrThrow(obj, "lastIndex", PropertyDescriptor {[[Writable]]: true, [[Enumerable]]: false, [[Configurable]]: false}).
   X(DefinePropertyOrThrow(obj, new Value('lastIndex'), Descriptor({
     Writable: Value.true,
     Enumerable: Value.false,
     Configurable: Value.false,
   })));
+
+  // 3. Return obj.
   return obj;
 }
 
@@ -152,4 +180,83 @@ export function EscapeRegExpPattern(P, _F) {
     }
   }
   return new Value(escaped);
+}
+
+// https://tc39.es/proposal-regexp-legacy-features/#sec-updatelegacyregexpstaticproperties
+export function UpdateLegacyRegExpStaticProperties(C, S, startIndex, endIndex, capturedValues) {
+  // 1. Assert C is an object that has a [[RegExpInput]] internal slot.
+  Assert(Type(C) === 'Object' && 'RegExpInput' in C);
+
+  // 2. Assert: Type(S) is String.
+  Assert(Type(S) === 'String');
+
+  // 3. Let len be the number of code units in S.
+  const len = S.stringValue().length;
+
+  // 4. Assert: startIndex and endIndex are integers such that 0 ≤ startIndex ≤ endIndex ≤ len.
+  Assert(Number.isInteger(startIndex) && Number.isInteger(endIndex)
+    && startIndex >= 0 && startIndex <= endIndex && endIndex <= len);
+
+  // 5. Assert: capturedValues is a List of Strings.
+  Assert(Array.isArray(capturedValues) && capturedValues.every((value) => Type(value) === 'String'));
+
+  // 6. Let n be the number of elements in capturedValues.
+  const n = capturedValues.length;
+
+  // 7. Set C.[[RegExpInput]] to S.
+  C.RegExpInput = S;
+
+  // 8. Set C.[[RegExpLastMatch]] to a String whose length is endIndex - startIndex and containing
+  // the code units from S with indices startIndex through endIndex - 1, in ascending order.
+  C.RegExpLastMatch = new Value(S.stringValue().substring(startIndex, endIndex));
+
+  if (n > 0) {
+    // 9. If n > 0, set C.[[RegExpLastParen]] to the last element of capturedValues.
+    C.RegExpLastParen = capturedValues[n - 1];
+  } else {
+    // 10. Else, set C.[[RegExpLastParen]] to the empty String.
+    C.RegExpLastParen = new Value('');
+  }
+
+  // 11. Set C.[[RegExpLeftContext]] to a String whose length is startIndex and containing
+  // the code units from S with indices 0 through startIndex - 1, in ascending order.
+  C.RegExpLeftContext = new Value(S.stringValue().substring(0, startIndex));
+
+  // 12. Set C.[[RegExpRightContext]] to a String whose length is len - endIndex and containing
+  // the code units from S with indices endIndex through len - 1, in ascending order.
+  C.RegExpRightContext = new Value(S.stringValue().substring(endIndex));
+
+  // 13. For each integer i such that 1 ≤ i ≤ 9
+  for (let i = 1; i <= 9; i += 1) {
+    const slotName = `RegExpParen${i}`;
+    if (i <= n) {
+      // a. If i ≤ n, set C.[[RegExpParen_i_]] to the _i_th element of capturedValues.
+      C[slotName] = capturedValues[i - 1];
+    } else {
+      // b. Else, set C.[[RegExpParen_i_]] to the empty String.
+      C[slotName] = new Value('');
+    }
+  }
+}
+
+// https://tc39.es/proposal-regexp-legacy-features/#sec-invalidatelegacyregexpstaticproperties
+export function InvalidateLegacyRegExpStaticProperties(C) {
+  // 1. Assert C is an object that has a [[RegExpInput]] internal slot.
+  Assert(Type(C) === 'Object' && 'RegExpInput' in C);
+
+  // 2. Set the value of the following internal slots of C to empty:
+  C.RegExpInput = undefined;
+  C.RegExpLastMatch = undefined;
+  C.RegExpLastParen = undefined;
+  C.RegExpLeftContext = undefined;
+  C.RegExpRightContext = undefined;
+  C.RegExpParen1 = undefined;
+  C.RegExpParen2 = undefined;
+  C.RegExpParen3 = undefined;
+  C.RegExpParen4 = undefined;
+  C.RegExpParen5 = undefined;
+  C.RegExpParen6 = undefined;
+  C.RegExpParen7 = undefined;
+  C.RegExpParen8 = undefined;
+  C.RegExpParen9 = undefined;
 }
