313d29ce9b18c9c3ad33c3687f68ae3b8c8f8ec3
Jack Works <5390719+Jack-Works@users.noreply.github.com>
2025-03-13 14:10:17 +0800

    fix: esvu

--- bin/engine262.js
+++ bin/engine262.js
@@ -4,11 +4,10 @@
 
 const { resolve } = require('node:path');
 const { spawnSync } = require('node:child_process');
-const { fileURLToPath } = require('node:url');
 
 const child = spawnSync(process.execPath, [
   ...process.execArgv, '--disable-warning=ExperimentalWarning', '--experimental-strip-types',
-  fileURLToPath(resolve('./engine262.mts')), ...process.argv.slice(2),
+  resolve(__dirname, './engine262.mts'), ...process.argv.slice(2),
 ], {
   stdio: 'inherit',
 });
